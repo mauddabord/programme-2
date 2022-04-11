2d8ff35affd247fb1d74d9c3639f5f50bc63911a
mauddabord <103410011+mauddabord@users.noreply.github.com>
2022-04-11 09:25:34 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        basic.showString("T=")
+        basic.showNumber(input.temperature())
+        basic.showString("DEGRES")
+    }
 })
